7a0b5dd5dca54a5aa72b5bb8c99203fd822fe75d
Zaheer Ali <zaheerrahman0@gmail.com>
2025-10-01 21:39:54 +0500

    image card

--- Frontend/src/utils/CardImage.js
+++ Frontend/src/utils/CardImage.js
@@ -1,31 +1,31 @@
 const content = {
   Everyone: {
     top: {
-      img: "./1.avif",
+      img: "./Card/1Card One.png",
       tage: "Tagline for Everyone One",
       pelegrah: "Paragraph for Everyone",
     },
 
     One: {
-      img: "./2.avif",
+      img: "./Card/2Card One.webp",
       tage: "Tagline for Everyone One",
       pelegrah: "Paragraph for Everyone",
     },
 
     Two: {
-      img: "./3.webp",
+      img: "./Card/3Card One.webp",
       tage: "Tagline for Everyone Two",
       pelegrah: "Paragraph for Everyone",
     },
 
     Three: {
-      img: "./4.avif",
+      img: "./Card/4Card One.webp",
       tage: "Tagline for Everyone Three",
       pelegrah: "Paragraph for Everyone",
     },
 
     Four: {
-      img: "./5.avif",
+      img: "./Card/5Card One.webp",
       tage: "Tagline for Everyone Four",
       pelegrah: "Paragraph for Everyone",
     },
@@ -33,33 +33,65 @@ const content = {
 
   Marketers: {
     top: {
-      img: "./6.avif",
+      img: "./Card/1Card two.webp",
       tage: "Tagline for Marketers One",
       pelegrah: "Paragraph for Marketers",
     },
 
     One: {
-      img: "./7.avif",
+      img: "./Card/2Card two.webp",
       tage: "Tagline for Marketers One",
       pelegrah: "Paragraph for Marketers",
     },
 
     Two: {
-      img: "./8.webp",
+      img: "./Card/3Card two.webp",
       tage: "Tagline for Marketers Two",
       pelegrah: "Paragraph for Marketers",
     },
 
     Three: {
-      img: "./9.avif",
+      img: "./Card/4Card two.webp",
       tage: "Tagline for Marketers Three",
       pelegrah: "Paragraph for Marketers",
     },
 
     Four: {
-      img: "./1.avif",
+      img: "./Card/5Card two.webp",
       tage: "Tagline for Marketers Four",
       pelegrah: "Paragraph for Marketers",
+    },
+  },
+
+   Sales: {
+    top: {
+      img: "./Card/1Card three.webp",
+      tage: "Tagline for Sales One",
+      pelegrah: "Paragraph for Sales One",
+    },
+
+    One: {
+      img: "./Card/2Card three.webp",
+      tage: "Tagline for Sales One",
+      pelegrah: "Paragraph for Sales One",
+    },
+
+    Two: {
+      img: "./Card/3Card three.webp",
+      tage: "Tagline for Sales Two",
+      pelegrah: "Paragraph for Sales Two",
+    },
+
+    Three: {
+      img: "./Card/4Card three.webp",
+      tage: "Tagline for Sales Three",
+      pelegrah: "Paragraph for Sales Three",
+    },
+
+    Four: {
+      img: "./Card/5Card three.webp",
+      tage: "Tagline for Sales Four",
+      pelegrah: "Paragraph for Sales Four",
     },
   },
 };
